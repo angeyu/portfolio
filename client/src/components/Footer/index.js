5f3b5ef6bf2e39677d500a3a49517e456a82a1de
unknown <angeyuangeyu@gmail.com>
2020-03-25 00:19:31 -0500

    ?????

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./style.css";
+
+function Footer() {
+    return (
+        <div>
+            <footer class="footer text-center clearfix p-3">Copyright&copy;</footer>
+        </div>
+    )
+}
+
+export default Footer;
